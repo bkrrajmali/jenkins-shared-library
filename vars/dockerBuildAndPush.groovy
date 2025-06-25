67a1141af315951215b09dabe2614fddeb2d9a32
bkrrajmali <bkrraj@gmail.com>
2025-06-25 15:20:18 +0530

    Update dockerBuildAndPush.groovy

--- vars/dockerBuildAndPush.groovy
+++ vars/dockerBuildAndPush.groovy
@@ -1,14 +1,3 @@
-// def call(String imageName, String tag) {
-//     log("Building Docker image: ${imageName}:${tag}")
-//     sh "docker build -t ${imageName}:${tag} ."
-
-//     log("Pushing Docker image to registry")
-//     withCredentials([usernamePassword(credentialsId: 'docker-cred', usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]) {
-//         sh "echo $DOCKER_PASS | docker login -u $DOCKER_USER --password-stdin"
-//         sh "docker push ${imageName}:${tag}"
-//     }
-// }
-
 def call(String imageName, String imageTag, String acrLoginServer, String acrUsername, String acrPasswordId) {
     withCredentials([usernamePassword(credentialsId: acrPasswordId, usernameVariable: 'ACR_USER', passwordVariable: 'ACR_PASS')]) {
         sh """
